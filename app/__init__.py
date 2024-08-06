a71b43d2fabf015252326fcbbf0104b518ae710c
rodriOy <r.oyonarte@alumno.um.edu.ar>
2024-08-06 15:09:22 -0300

    fixes para el FE

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,14 @@
 from flask import Flask
-from config import Config
 from flask_cors import CORS
-
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
 
     # Habilitar CORS globalmente
     CORS(app)
 
     from app.routes import bp as routes_bp
     app.register_blueprint(routes_bp)
-
-
 
     return app
